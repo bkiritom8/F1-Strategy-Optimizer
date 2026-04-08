89c997ce91432e74a5064be8d41acd6f3e9f4692
Ajith Srikanth <ajithsri3103@gmail.com>
2026-04-08 11:34:47 -0400

    refactor(frontend): remove legacy Vercel artifacts and finalize Firebase configuration

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/react"
+
 import App from './App';
 import './index.css';
 
@@ -16,8 +15,6 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Analytics />
-      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>
 );
